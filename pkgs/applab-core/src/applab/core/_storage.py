3c16be317ef53680df089ee377d67e58fed1b5a4
Chen Peng <chen56@msn.com>
2026-01-07 13:55:54 +0800

    atom save

--- pkgs/applab-core/src/applab/core/_storage.py
+++ pkgs/applab-core/src/applab/core/_storage.py
@@ -19,15 +19,68 @@ class JsonStorage[T: BaseModel]:
         return self.model.model_validate_json(json_string)
 
     def save(self, doc: T):
-        self.path.parent.mkdir(parents=True, exist_ok=True)
+        atomic_save_text(self.path, doc.model_dump_json(indent=4))
 
-        tmp = self.path.with_suffix(".json.tmp")
 
-        # 1. 写临时文件
-        with open(tmp, "w", encoding="utf-8") as f:
-            f.write(doc.model_dump_json(indent=4))
-            f.flush()
+# ---------------------------
+# 文本版本
+# ---------------------------
+def atomic_save_text(
+        path: Path,
+        text: str,
+        *,
+        encoding: str = "utf-8",
+        fsync: bool = True,
+) -> None:
+    """
+    Atomically save text or JSON-serializable data to a file.
+
+    """
+    path = path.resolve()
+    path.parent.mkdir(parents=True, exist_ok=True)
+
+    tmp = _temp_file_path(path)
+
+    with open(tmp, "w", encoding=encoding) as f:
+        f.write(text)
+        f.flush()
+        if fsync:
             os.fsync(f.fileno())
 
-        # 2. 原子替换
-        os.replace(tmp, self.path)
+    os.replace(tmp, path)
+
+
+# ---------------------------
+# 二进制版本
+# ---------------------------
+def atomic_save_bytes(
+        path: Path,
+        data: bytes,
+        *,
+        fsync: bool = True,
+) -> None:
+    """
+    Atomically save binary data to a file.
+    """
+    path = path.resolve()
+    path.parent.mkdir(parents=True, exist_ok=True)
+
+    tmp = _temp_file_path(path)
+
+    with open(tmp, "wb") as f:
+        f.write(data)
+        f.flush()
+        if fsync:
+            os.fsync(f.fileno())
+
+    os.replace(tmp, path)
+
+
+def _temp_file_path(path: Path) -> Path:
+    """
+    注意：
+    1. 未使用tempfile.NamedTemporaryFile工具,因为它默认目录为/var/folders/.../T/tmpxxxx，而path可能在云盘，
+    跨文件系统的os.replace不是原子操作。
+    2. tmp文件即便失败也不用清理，下次重新覆盖和os.replace
+    """
+    return path.with_name(f".{path.name}.tmp")

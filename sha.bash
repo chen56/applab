27cbb9d21c685ee3b9f3293f2e8c544cd7eb5a14
Chen Peng <chen56@msn.com>
2026-01-28 22:06:54 +0800

    devx(ai): add ai skills: find-skills planning-with-files

--- sha.bash
+++ sha.bash
@@ -36,8 +36,6 @@ publish() {
 }
 
 sync() (
-  _run rm -rf .gemini/skills && ln -fs ../.agent/skills .gemini/skills
-  _run rm -rf .qwen/skills   && ln -fs ../.agent/skills .qwen/skills
 
   _run uv sync --all-extras --all-groups
   _run rsync -av --delete .agent build/
@@ -46,8 +44,14 @@ sync() (
 
 # 太慢了，还有输入选项，单独安装吧
 sync_skills() {
-  _run npx skills add https://github.com/anthropics/skills --skill skill-creator --agent gemini-cli
-  _run npx skills add https://github.com/vercel-labs/skills --skill find-skills  --agent gemini-cli
+  # find . -type d -name 'skills' -not -path './.gemini/*' -not -path './.qwen/*' -not -path './.agent/*' -not -path './.agents/*' -exec dirname {} \; | sort -u
+
+  _run npx skills add --agent gemini-cli -y https://github.com/anthropics/skills --skill skill-creator
+  _run npx skills add --agent gemini-cli -y https://github.com/vercel-labs/skills --skill find-skills
+  _run npx skills add --agent gemini-cli -y https://github.com/othmanadi/planning-with-files --skill planning-with-files
+
+  _run rm -rf .gemini/skills && ln -fs ../.agent/skills .gemini/skills
+  _run rm -rf .qwen/skills   && ln -fs ../.agent/skills .qwen/skills
 
 }
 
